b830230e1762190e36a307d159e9e5ca2cbd6144
chri3092 <109158615+chri3092@users.noreply.github.com>
2023-07-28 16:04:56 +0200

    end

---
+++ extra.js
@@ -0,0 +1,43 @@
+/* 1 EXTRA */
+
+window.onload = function () {
+    const removeTwitter = document.querySelector('aside div:nth-child(3) li:nth-child(2)')
+
+    removeTwitter.remove()
+    
+    /* 2 EXTRA */
+
+    const links = document.querySelectorAll('.jumbotron a, .stretched-link')
+    
+    console.log(links)
+    
+    for(const link of links) {
+        link.addEventListener("click", function (event){
+            console.log(event.target)
+
+            /* event.target.parentNode.remove() */
+
+            event.target.closest('.row, .jumbotron').remove()
+        })
+    }
+
+    /* 2 EXTRA */
+
+    const autorsSelect = document.querySelectorAll('.blog-post p.blog-post-meta a')
+
+    console.log(autorsSelect)
+
+    for (const autor of autorsSelect) {
+        autor.addEventListener("mouseover", function (event){
+            alert(event.target.innerText)
+        })
+    }
+}
+
+
+    
+    
+
+
+
+
